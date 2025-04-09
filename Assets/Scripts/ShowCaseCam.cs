76ba29a711c68a3e270993ecf49bfe501a7e70ea
AidanKleine <aidankleine@outlook.com>
2025-04-09 18:30:00 -0400

    more temp cam edits

--- Assets/Scripts/ShowCaseCam.cs
+++ Assets/Scripts/ShowCaseCam.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class ShowCaseCam : MonoBehaviour
 {
 
-    float spd = 50f;
-    Rigidbody rb;
-
-    private void Start()
-    {
-        rb= GetComponent<Rigidbody>();
-    }
+    float spd = 10f;
     private void Update()
     {
         Move();
@@ -20,24 +14,37 @@ public class ShowCaseCam : MonoBehaviour
 
     private void Move()
     {
-
+        float hor = 0f;
+        float forw = 0f;
+        float ver = 0f;
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("running");
-            rb.velocity = spd * transform.right;
+            hor = spd;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = -spd * transform.right;
+            hor = -spd;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = spd * transform.forward;
+            forw = spd;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.velocity = -spd * transform.forward;
+            forw = -spd;
         }
+        if (Input.GetKey(KeyCode.I))
+        {
+            ver = spd;
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            ver = -spd;
+        }
+        transform.right += new Vector3(hor, 0f, 0f);
+        transform.forward += new Vector3(0f, 0f, forw);
+        transform.right += new Vector3(0f, ver, 0f);
     }
 
     void Turn()
@@ -49,6 +56,14 @@ public class ShowCaseCam : MonoBehaviour
         else if (Input.GetKey(KeyCode.Q))
         {
             transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * spd, Space.World);
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * spd, Space.World);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * spd, Space.World);
         }
     }
 }
